7dc21ed9c6362086064f5896c7a33050d17636b1
Никита Левашов <sh4rpy@ya.ru>
2020-09-10 19:40:14 +0300

    added a new TODO

--- recipes/urls.py
+++ recipes/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 
 urlpatterns = [
+    # TODO: поработать над мэппингом
     path('', views.index, name='index'),
     path('recipes/<int:recipe_id>/', views.recipe_detail, name='recipe_detail'),
     path('subscriptions/', views.follow_index, name='follow_index'),
